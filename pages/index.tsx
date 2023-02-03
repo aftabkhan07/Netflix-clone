8ecef4d0200ccbb383b262f30641f0a0d5b830e9
aftabkhan07 <khanaftaba1@gmail.com>
2023-02-03 23:32:41 +0530

    Api part(1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,41 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import { Movie } from '../typings'
 import requests from '../utils/requests'
 
-const Home = ({ netflixOriginals }) => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   console.log(netflixOriginals)
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>NETFLIX</title>
         <link rel="icon" href="/favicon.ico" />
-      
       </Head>
       <Header />
       <main>
         <Banner/>
       </main>
-      
     </div>
   )
 }
