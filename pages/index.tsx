dfcc4a0d4036707408078a7cc2454bedd8e8ec6a
aftabkhan07 <khanaftaba1@gmail.com>
2023-08-13 22:24:35 +0530

    plans page and stripe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
+import { getProducts, Product } from '@stripe/firestore-stripe-payments'
 import Head from 'next/head'
+import { useRecoilValue } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
+import Plans from '../components/Plans'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
+// import useList from '../hooks/useList'
+// import useSubscription from '../hooks/useSubscription'
+import payments from '../lib/stripe'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
-import useAuth from '../hooks/useAuth'
-import { useRecoilValue } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
-import Modal from '../components/Modal'
 
-// am back 26_02_22
 interface Props {
   netflixOriginals: Movie[]
   trendingNow: Movie[]
@@ -19,6 +23,7 @@ interface Props {
   horrorMovies: Movie[]
   romanceMovies: Movie[]
   documentaries: Movie[]
+  products: Product[]
 }
 
 const Home = ({
@@ -30,34 +35,44 @@ const Home = ({
   romanceMovies,
   topRated,
   trendingNow,
+  products,
 }: Props) => {
-
-  const { loading } = useAuth()
+  const { loading, user } = useAuth()
   const showModal = useRecoilValue(modalState)
+  const subscription = false
+  const movie = useRecoilValue(movieState)
 
-  if (loading) return null
+  if (loading || subscription === null) return null
+
+  if (!subscription) return <Plans products={products} />
 
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
-        <title>AFTAFLIX</title>
+        <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
-      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
-        <Banner netflixOriginals={netflixOriginals}/>
-        <section className='md:space-y-24'>
-            <Row title="Trending Now" movies={trendingNow} />
-            <Row title="Top Rated" movies={topRated} />
-            <Row title="Action Thrillers" movies={actionMovies} />
-            {/* My List */}
-            <Row title="Comedies" movies={comedyMovies} />
-            <Row title="Scary Movies" movies={horrorMovies} />
-            <Row title="Romance Movies" movies={romanceMovies} />
-            <Row title="Documentaries" movies={documentaries} />
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+        <Banner netflixOriginals={netflixOriginals} />
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {showModal && <Modal/>}
+      {showModal && <Modal />}
     </div>
   )
 }
@@ -65,6 +80,12 @@ const Home = ({
 export default Home
 
 export const getServerSideProps = async () => {
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  })
+    .then((res) => res)
+    .catch((error) => console.log(error.message))
 
   const [
     netflixOriginals,
@@ -96,8 +117,7 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
+      products,
     },
   }
 }
-
-
