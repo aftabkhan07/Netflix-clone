46751ea5ae2008809029a7ad2019a22797380f76
vinayak <aftab.k@apicalin.com>
2023-08-14 16:08:09 +0530

    minor change in Plan page

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -25,14 +25,14 @@ function Plans({ products }: Props) {
   return (
     <div>
       <Head>
-        <title>Netflix</title>
+        <title>Aftaflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header className="border-b border-white/10 bg-[#141414]">
         <Link href="/">
           <img
-            src="https://rb.gy/ulxxee"
+            src="https://fontmeme.com/permalink/230207/5cff4b7fecd9021f0b04674d015aa0e3.png"
             alt="Netflix"
             width={150}
             height={90}
