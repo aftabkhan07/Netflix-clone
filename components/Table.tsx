3e770511d15a33f40ce06a51499c1c8379cd6889
aftabkhan07 <khanaftaba1@gmail.com>
2023-08-23 00:30:18 +0530

    minor change

--- components/Table.tsx
+++ components/Table.tsx
@@ -9,7 +9,7 @@ interface Props {
 function Table({ products, selectedPlan }: Props) {
   return (
     <table>
-      <tbody className="divide-y divide-[red]">
+      <tbody className="divide-y divide-[grey]">
         <tr className="tableRow">
           <td className="tableDataTitle">Monthly price</td>
           {products.map((product) => (
